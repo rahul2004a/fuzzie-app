974d62c94440714d7541ad95aece5b962c05aac7
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-04-02 16:00:51 +0530

    Sidebar and Dark mode / light mode setup

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import MenuOptions from "@/components/sidebar";
 type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
+      <MenuOptions />
       <div className="w-full">{props.children}</div>
     </div>
   );
