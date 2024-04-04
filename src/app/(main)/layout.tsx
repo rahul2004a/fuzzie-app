fc3b6843cf6ad0dd61043a4dce343a0b6cfd7888
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-04-04 16:59:13 +0530

    Setting Page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import MenuOptions from "@/components/sidebar";
+import InfoBar from "@/components/infobar";
+
 type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
       <MenuOptions />
-      <div className="w-full">{props.children}</div>
+      <div className="w-full">
+        <InfoBar />
+        {props.children}
+      </div>
     </div>
   );
 };
